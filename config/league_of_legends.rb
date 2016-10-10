4adcb02a9952dfc1cd24fc2c0286413c13839240
Bruno Arueira <contato@brunoarueira.com>
2016-10-10 19:23:29 -0300

    Change the call for the env variables to easy debug and fail fast

--- config/league_of_legends.rb
+++ config/league_of_legends.rb
@@ -1,5 +1,5 @@
 LeagueOfLegends.config do |config|
-  config.api_key = ENV['LOL_API_KEY']
+  config.api_key = ENV.fetch('LOL_API_KEY')
   config.region = "euw"
-  config.redis_url = ENV['REDIS_PROVIDER'] || 'redis://localhost:6379/'
+  config.redis_url = ENV.fetch('REDIS_PROVIDER', 'redis://localhost:6379/')
 end
